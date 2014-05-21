690bd608c80173c48fc6c24b4bae15def06a0234
infonalysis <urban.martin@ucdenver.edu>
2014-05-20 21:28:36 -0600

    plot3: process data for plot3

---
+++ plot3.R
@@ -0,0 +1,21 @@
+## Read in data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Calculate annual totals
+emission_totals <- data.frame(year=as.integer(), type=as.character(), total_emissions=as.numeric(), stringsAsFactors=FALSE)
+
+for (ayear in unique(NEI$year)) {  ## for each year
+	for (atype in as.character(unique(NEI$type))) {  ## and for each type
+  		total <- sum(with(NEI, subset(Emissions, fips=="24510" & year==ayear & type==atype)))  ## calculate total emissions for baltimore city
+  		emission_totals[nrow(emission_totals)+1,] <- t(c(ayear, atype, total))  ## write to results data frame
+	}
+}
+
+##  draw graph and save to png
+png(file="plot3.png", width=504, height=504, bg="white")  ## engage png device
+
+
+
+## barplot(emission_totals$total_emissions, names.arg=emission_totals$year, xlab="Year", ylab="PM 2.5 Emissions (tons)", main="Total PM 2.5 Emissions in Baltimore City, MD")  ## barplot
+dev.off()  # ... and close device
